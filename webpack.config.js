36adf7b5ec39a14b5751d959b2c2189421e5f975
SidKH <my.mail.kharkov@gmail.com>
2020-08-12 23:05:45 +0300

    ESlint

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,33 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
-  mode: 'production',
+  entry: "./src/index.js",
+  mode: "production",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    library: 'yesno',
-    libraryTarget: 'commonjs2'
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    library: "yesno",
+    libraryTarget: "commonjs2",
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
+          },
+          "eslint-loader",
+        ],
       },
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      }
-    ]
-  }
-}
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
